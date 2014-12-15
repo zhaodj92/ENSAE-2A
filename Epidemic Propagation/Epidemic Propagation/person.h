1e95cf42c155cee4baa0e0d9924f119037228224
zhaodj92 <zhaodj92@126.com>
2014-12-15 22:42:52 +0800

    Revert "Revert "beginning of all things""
    This reverts commit 9daf6eeacfc02dec0d808ce32eb0215158387ca5.

---
+++ Epidemic Propagation/Epidemic Propagation/person.h
@@ -0,0 +1,54 @@
+#ifndef Person_H
+#define Person_H
+
+#include <string.h>
+
+class Person
+{
+public:
+	int id;				//identification of the person
+	char age;			//"child" or "adult" or "old"
+
+class Buiding::public Person{
+public:
+	int x;				//coordinate
+	int y;
+	char type;			//home school bureau store bus...
+	int buildingID;
+	int numOfSicks;
+	
+	void functionInBuiding()
+}
+
+class Trajectory::public Person{
+public:
+	int t1;				//for every n between 1 and 10, tn=i means: change his position to building which identification is i
+	int t2;				//changes take place at the moment tn
+	int t3;				//t1 corresponds to 6:00 am; t2= 7:00am...
+	int t4;
+	int t5;
+	int t6;
+	int t7;
+	int t8;
+	int t9;
+	int t10;
+	int t11;
+	int t12;
+
+	void functionInTrajectory()
+
+}
+
+class Health::public Person{
+public:
+	bool isHealthful;	//1=healthful 0=sick
+	float p;			//probability to be infected
+
+	void probability(age, Building.type, Building.numOfSicks)
+}
+
+	void functionInPerson()
+	
+}
+
+#endif
